6eee9d3010fc33399f8e71e3db42f731df8aec0d
istojan <stoleilieski@gmail.com>
2020-07-13 18:32:04 +0200

    Updated configuration

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ settings = {
     "text_corpus_directory": "corpus/books",
     "male_names_file_path": "other/male_names.txt",
     "female_names_file_path": "other/female_names.txt",
-    "output_file_path": "output/language_model_books2_1500_v17.pickle",
+    "output_file_path": "output/big_test_trimmed_first-level-10_other-levels-2.pickle",
     "dictionary_file_path": "other/macedonian-dict"
 }
 
