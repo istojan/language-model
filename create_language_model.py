f29bcc8502eef578b0d361a42790bbf3ae8cfe90
istojan <stoleilieski@gmail.com>
2020-07-13 18:32:45 +0200

    Added docs for creation of language model

--- create_language_model.py
+++ create_language_model.py
@@ -1,4 +1,5 @@
-from text_helpers.text_tokenizer import extract_all_sentences_from_files, convert_sentences_to_list_of_words, read_names
+from helpers.text_tokenizer import extract_all_sentences_from_files, convert_sentences_to_list_of_words, read_names
+from helpers.utils import get_elapsed_time
 from word_occurrences_model.word_occurrence_model_builder import build_word_occurrence_model
 from language_model.language_model import LanguageModel
 from os import path
@@ -8,57 +9,46 @@ import time
 import sys
 
 
-def load_language_model(path):
-    with open(path, "rb") as pickle_file:
-        language_model = pickle.load(pickle_file)
-        return language_model
-
-
 def save_language_model(language_model_data, path):
+    """
+    Saves the language model in a pickle file to be later used by the language model service.
+    """
 
     start_time = time.time()
-    print("Message=\"Starting to save language model.\"")
+    print("Message=\"Starting to save language model\"")
 
     with open(path, "wb") as pickle_file:
         pickle.dump(language_model_data, pickle_file)
 
-    print("ElapsedTime={}, Message=\"Finished saving language model.\"".format(format(time.time() - start_time, ".2f")))
+    print("ElapsedTime={}, Message=\"Finished saving language model.\"".format(get_elapsed_time(start_time)))
 
 
 if __name__ == '__main__':
 
-    total_start_time = time.time()
-    print("Message=\"Starting program for language model training.\"")
+    model_name = settings["output_file_path"]
 
-    if path.exists(settings["output_file_path"]):
-        print("Message=\"Specified language model already exists, cancelling training and proceeding to load it.\"")
-        language_model_data = load_language_model(settings["output_file_path"])
-        language_model = LanguageModel()
-        language_model.load_model_data(language_model_data)
-    else:
-        sentences = extract_all_sentences_from_files(settings["text_corpus_directory"])
-
-        names_set = read_names(settings["male_names_file_path"], settings["female_names_file_path"])
-
-        list_of_sentences_as_words = convert_sentences_to_list_of_words(sentences, names_set)
-
+    if path.exists(model_name):
+        print("ModelName=\"{}\", Message=\"A language model with specified name already exists. Use a different name or delete the old model\"".format(model_name))
         sys.exit(0)
-        # sentences = ["утрово изгреа сонце на исток", "ова сонце утрово изгреа на исток сега", "ех сестро"]
 
-        word_occurrence_model = build_word_occurrence_model(settings["n_gram_level"], list_of_sentences_as_words)
+    start_time = time.time()
+    print("ModelName=\"{}\", Message=\"Starting language model training\"".format(model_name))
 
-        language_model = LanguageModel()
-        language_model.build_language_model(word_occurrence_model)
+    sentences = extract_all_sentences_from_files(settings["text_corpus_directory"])
 
-        model_data = language_model.prepare_model_data_for_saving()
-        save_language_model(model_data, settings["output_file_path"])
+    # TODO: Delete
+    print(sentences[:20])
 
-    language_model.test_language_model()
+    names_set = read_names(settings["male_names_file_path"], settings["female_names_file_path"])
 
-    print("TotalElapsedTime={}, Message=\"Program has finished.\"".format(format(time.time() - total_start_time, ".2f")))
+    list_of_sentences_as_words = convert_sentences_to_list_of_words(sentences, names_set)
 
+    word_occurrence_model = build_word_occurrence_model(list_of_sentences_as_words)
 
+    language_model = LanguageModel()
+    language_model.build_language_model(word_occurrence_model)
 
+    model_data = language_model.prepare_model_data_for_saving()
+    save_language_model(model_data, model_name)
 
-
-
+    print("TotalElapsedTime={}, ModelName=\"{}\", Message=\"Finished language model training\"".format(get_elapsed_time(start_time), model_name))
--- language_model/language_model.py
+++ language_model/language_model.py
@@ -1,6 +1,7 @@
-from operator import attrgetter
 import time
+from operator import attrgetter
 from language_model.probability_node import ProbabilityNode
+from helpers.utils import get_elapsed_time
 
 
 class LanguageModel:
@@ -11,9 +12,12 @@ class LanguageModel:
         self.sorted_probability_nodes = list()
 
     def build_language_model(self, word_occurrences_model):
+        """
+        From a given word occurrences model, build a probability language model.
+        """
 
         start_time = time.time()
-        print("Message=\"Starting to train a language model from a word occurrences model.\"")
+        print("Message=\"Starting to train a language model from a word occurrences model\"")
 
         for word, word_occurrence_node in word_occurrences_model.items():
             word_count = word_occurrence_node.word_count
@@ -24,23 +28,23 @@ class LanguageModel:
 
         self.calculate_nodes_probability(self.probability_nodes, self.total_words_count)
         self.sorted_probability_nodes = sort_probability_list(self.probability_nodes.values())
-        # self.sorted_probability_nodes = self.sorted_probability_nodes[:1000]
 
-        print("ElapsedTime={}s, TotalWordsCount={}, TotalDistinctWordsCount={}, Message=\"Finished training language model from word occurrences model\"".format(format(time.time() - start_time, ".2f"), self.total_words_count, len(self.probability_nodes)))
+        print("ElapsedTime={}, TotalWordsCount={}, TotalDistinctWordsCount={}, Message=\"Finished training language model from word occurrences model\""
+              .format(get_elapsed_time(start_time), self.total_words_count, len(self.probability_nodes)))
 
-    def build_word_occurrence_probability_node(self, word, word_count, word_occurrences):
+    def build_word_occurrence_probability_node(self, base_word, base_word_count, children_word_occurrences):
 
-        probability_node = ProbabilityNode(word, word_count)
+        probability_node = ProbabilityNode(base_word, base_word_count)
 
-        if len(word_occurrences) == 0:
+        if len(children_word_occurrences) == 0:
             return probability_node
 
-        for word, word_occurrences_node in word_occurrences.items():
-            word_count = word_occurrences_node.word_count
-            probability_node.word_count += word_count
+        for child_word, word_occurrences_node in children_word_occurrences.items():
+            child_word_count = word_occurrences_node.word_count
+            probability_node.word_count += child_word_count
 
-            child_probability_node = self.build_word_occurrence_probability_node(word, word_count, word_occurrences_node.children_nodes)
-            probability_node.children_nodes[word] = child_probability_node
+            child_probability_node = self.build_word_occurrence_probability_node(child_word, child_word_count, word_occurrences_node.children_nodes)
+            probability_node.children_nodes[child_word] = child_probability_node
 
         self.calculate_nodes_probability(probability_node.children_nodes, probability_node.word_count)
 
@@ -50,24 +54,13 @@ class LanguageModel:
 
         return probability_node
 
-    def calculate_nodes_probability(self, probability_nodes, total_words_count):
-        for word, probability_node in probability_nodes.items():
-            probability_node.calculate_probability(total_words_count)
+    def calculate_nodes_probability(self, children_probability_nodes, total_words_count):
 
-    def test_language_model(self):
-        print([ [node] for node in self.sorted_probability_nodes[:5]])
-        # for node2 in self.sorted_probability_nodes[:5]:
-            # print("Word: {}, Count: {}, Prob: {}, Children: {}, ProbChildren: {}".format(node2.word, node2.word_count, node2.probability, len(node2.children_nodes), len(node2.sorted_probability_nodes)))
-            # print([[node.word, node.probability] for node in node2.sorted_probability_nodes[:5]])
+        for word, probability_node in children_probability_nodes.items():
+            probability_node.calculate_probability(total_words_count)
 
     def prepare_model_data_for_saving(self):
         return [self.total_words_count, self.probability_nodes, self.sorted_probability_nodes]
-
-    def load_model_data(self, model_data):
-        print("Loading model data with total words: {}".format(model_data[0]))
-        self.total_words_count = model_data[0]
-        self.probability_nodes = model_data[1]
-        self.sorted_probability_nodes = model_data[2]
 
 
 def sort_probability_list(probability_nodes):
